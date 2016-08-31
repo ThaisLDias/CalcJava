b2b947530acf8ae0fe6222632a6081b21db66175
Thais Dias <dragonssaur@gmail.com>
2016-08-30 23:57:20 -0300

    :dancers:

--- app/src/main/java/com/example/danielcavalli/calc/MainActivity.java
+++ app/src/main/java/com/example/danielcavalli/calc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.danielcavalli.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -43,20 +44,76 @@ public class MainActivity extends AppCompatActivity {
         {
             case"+":
                 t.setText(String.valueOf(parameter + parameter2));
-            break;
--
+                break;
+
             case"-":
                 t.setText(String.valueOf(parameter - parameter2));
-            break;
+                break;
 
             case"*":
                 t.setText(String.valueOf(parameter * parameter2));
-            break;
+                break;
 
             case"/":
                 t.setText(String.valueOf(parameter / parameter2));
-            break;
+                break;
+
+            case"%":
+                Double a = parameter * (parameter2/100);
+                Log.d("Main Activity", a.toString());
+                t.setText(a.toString());
+                break;
         }
+    }
+    public void clearLast(View v)
+    {
+        TextView t = (TextView) findViewById(R.id.textView);
+        String b = t.getText().toString();
+        String a = b.substring(0,b.length() - 1);
+        t.setText(a);
+    }
+    public void clearText(View v)
+    {
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("");
+    }
+    public void clearAll(View v)
+    {
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("");
+        parameter = Double.NaN;
+        parameter2 = Double.NaN;
+        operator = "";
+    }
+
+    public void oneAbove (View v)
+    {
+        TextView t = (TextView) findViewById(R.id.textView);
+        Double a = Double.parseDouble(t.getText().toString());
+        a = 1/a;
+        Log.e("Main Activity", a.toString());
+        t.setText(a.toString());
+    }
+
+
+    public void invertSignal (View v)
+    {
+        TextView t = (TextView) findViewById(R.id.textView);
+        Double a = Double.parseDouble(t.getText().toString());
+
+        if(a >= 0)
+            t.setText("-" + a.toString());
+        else {
+            String b = a.toString();
+            b = b.substring(1);
+            t.setText(b);
+        }
+    }
+    public void squareRoot (View v) {
+        TextView t = (TextView) findViewById(R.id.textView);
+        Double a = Double.parseDouble(t.getText().toString());
+        a = Math.sqrt(a);
+        t.setText(a.toString());
     }
 
 }
